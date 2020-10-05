b4b4d5c4205eead5de2648b6417fa72d4c1018b8
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-10-05 10:27:32 -0600

    Intento crear un deploy para probarlo arriba
    MUY CARO LOS HOSTINGS

---
+++ build/precache-manifest.fa64b5d05ceabe70c439539b0f60e1bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b86e54834c2cf7e9f79e05905bb6db0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a97d1102bd21ad706b8",
+    "url": "/static/css/main.fb8cee43.chunk.css"
+  },
+  {
+    "revision": "4c0053d5fdb4bb25ce01",
+    "url": "/static/js/2.5e6c6552.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.5e6c6552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a97d1102bd21ad706b8",
+    "url": "/static/js/main.8566c8e6.chunk.js"
+  },
+  {
+    "revision": "6d0710caf010806f0826",
+    "url": "/static/js/runtime-main.bac25c6c.js"
+  },
+  {
+    "revision": "dc2a00d9660f237c05796d0ce838a3ef",
+    "url": "/static/media/videollamada.dc2a00d9.webp"
+  }
+]);
